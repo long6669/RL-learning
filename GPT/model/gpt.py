5d7c1d735b17a3e5abb8ea1e9d4b078940652d69
long-sensetime <jinlong@sensetime.com>
2025-12-24 20:14:50 +0800

    fix: gpt

--- GPT/model/gpt.py
+++ GPT/model/gpt.py
@@ -1,7 +1,7 @@
 from dataclasses import dataclass
 import math
+from operator import inv
 
-from numpy import square
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -69,9 +69,101 @@ class CasualSelfAttetion(nn.Module):
 
         enable_gqa = self.n_head != self.n_kv_head
         if kv_cache is None or Tq == Tk:
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=True, enable_gqa=enable_gqa)
+        
+        elif Tq == 1:
+            y = F.scaled_dot_product_attention(q, k, v, is_causal=False, enable_gqa=enable_gqa)
+
+        else:
+            attn_mask = torch.zeros((Tq, Tk), dtype=torch.bool, device=q.device) # True = keep, False = mask
+            prefix_len = Tk - Tq
+            attn_mask[:, :prefix_len] = True
+            attn_mask[:, prefix_len:] = torch.tril(torch.ones(Tq, Tq), dtype=torch.bool, device=q.device)
+            y = F.scaled_dot_product_attention(q, k, v, attn_mask=attn_mask, enable_gqa=enable_gqa)
+
+        y = y.transpose(1, 2).coutiguous().view(B, T, -1)
+        y = self.o_proj(y)
+
+        return y
+            
+class MLP(nn.Module):
+    def __init__(self, config):
+        super().__init__()
+        self.c_fc = nn.Linear(config.n_embed, 4*config.n_embed, bias=False)
+        self.c_proj = nn.Linear(4*config.n_embed, config.n_embed, bias=False)
+
+    def forward(self, x):
+        x = self.c_fc(x)
+        x = F.relu(x).square()
+        x = self.c_proj(x)
+        return x
+
+class Block(nn.Module):
+    def __init__(self, config, layer_idx):
+        super().__init__()
+        self.attn = CasualSelfAttetion(config, layer_idx)
+        self.mlp = MLP(config)
+    def forward(self, x, cos_sin, kv_cache):
+        x = x + self.attn(RMS_Norm(x), cos_sin, kv_cache)
+        x = x + self.mlp(RMS_Norm(x))
+        return x
+
+class GPT(nn.Module):
+    def __init__(self, config, pad_vocab_size_to=64):
+        super().__init__()
+        self.config = config
+        padded_vocab_size = ((config.vocab_size + pad_vocab_size_to - 1) // pad_vocab_size_to) * pad_vocab_size_to
+        if padded_vocab_size != config.vocab_size:
+            print(f"Padding vocab_size from {config.vocab_size} to {padded_vocab_size} to be divisible by {pad_vocab_size_to}")
+        
+        self.transformer = nn.ModuleDict({
+            "wte": nn.Embedding(padded_vocab_size, config.n_embed),
+            "h": nn.ModuleList([Block(config, layer_idx) for layer_idx in range(config.n_layer)])
+        })
+
+        self.lm_head = nn.Linear(config.n_embed, padded_vocab_size, bias=False)
+
+        self.rotary_seq_len = config.sequence_len*10
+        head_dim = config.n_embed // config.n_head
+        cos, sin = self._precompute_rotary_embeddings(self.rotary_seq_len, head_dim)
+        self.register_buffer("cos", cos, persistent=False)
+        self.register_buffer("sin", sin, persistent=False)
+    
+    def init_weghts(self):
+        self.apply(self._init_weights)
+        torch.nn.init.zeros_(self.lm_head.weight)
+        for block in self.transformer.h:
+            torch.nn.init.zeros_(block.mlp.c_proj.weight)
+            torch.nn.init.zeros_(block.attn.o_proj.weight)
+        
+        head_dim = self.config.n_embed
+        cos, sin = self._precompute_rotary_embeddings(self.rotary_seq_len, head_dim)
+        self.cos, self.sin = cos, sin
+        if self.transformer.wte.weight.device.type == "cuda":
+            self.transformer.wte.to(dtype=torch.bfloat16)
             
 
+    def _init_weights(self, module):
+        if isinstance(module, nn.Linear):
+            fan_out = module.weight.size(0)
+            fan_in = module.weight.size(1)
+            std = 1.0 / math.sqrt(fan_in) * min(1.0, math.sqrt(fan_out/fan_in))
+            torch.nn.init.normal_(module.weight, mean=0.0, std=std)
+            if module.bias is not None:
+                torch.nn.init.zeros_(module.bias)
+        elif isinstance(module, nn.Embedding):
+            torch.nn.init.normal_(module.weight, mean=0.0, std=1.0)  
 
-
-
-
+    def _precompute_rotary_embeddings(self, seq_len, head_dim, base=10000, device=None):
+        if device is None:
+            device = self.transformer.wte.weight.device
+        channel_range = torch.arange(0, head_dim, 2, dtype=torch.float32, device=device)
+        inv_freq = 1.0 / (base ** (channel_range / head_dim))
+        t = torch.arange(seq_len, dtype=torch.float32, device=device)
+        freqs = torch.outer(t, inv_freq)
+        cos, sin = freqs.cos(), freqs.sin()
+        cos, sin = cos.bfloat16(), sin.bfloat16()
+        cos, sin = cos[None, :, None, :], sin[None, :, None, :]
+        return cos, sin
+    
+    def forward(self, )
